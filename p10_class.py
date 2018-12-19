c6fc144fe2f45a0b44631df7cc53065ba656a2a7
cosmin_louis <32954953+cosmineala@users.noreply.github.com>
2018-12-19 12:47:14 +0200

    0.1
    + clasa celula b

---
+++ p10_class.py
@@ -0,0 +1,48 @@
+
+class celula(object):
+	
+	def __init__(self , stare , x , y ) :
+		
+		self.srate = stare
+		self.next_gen = stare 
+		self.x = x
+		self.y = y
+
+	def set_stare( stare ) : 
+
+		self.stare = stare
+
+	def get_stare():
+		
+		return self.stare
+
+	def set_next_gen( next_gen ) :
+
+		self.next_gen = next_gen
+
+	def get_next_gen() :
+
+		return self.next_gen
+
+	def next_gen() :
+		
+		self.stre = self.next_gen
+
+	def set_x( x ) :
+
+		self.x = x 
+
+	def get_x() :
+
+		return self.x
+
+	def set_y( y ) :
+
+		self.y = y
+
+	def get_y() :
+
+		return self.y
+
+
+		
