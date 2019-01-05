ec789e10ca3feed7cf5c03e828143d97cc0ae6fe
cosmin_louis <32954953+cosmineala@users.noreply.github.com>
2019-01-06 01:28:09 +0200

    0.7
    + impartire algoritm evolutie
    - ies din spatiul alocat( index prea mare , linia 208 )

--- p10_class.py
+++ p10_class.py
@@ -10,6 +10,8 @@ class celula(object):
 		self.next_gen = stare 
 		self.x = x
 		self.y = y
+		self.photo1 = PhotoImage(file='c1.gif')
+		self.photo0 = PhotoImage(file='c0.gif')
 
 		t_celula = Label( root , image = self.get_poza() )
 
@@ -21,13 +23,10 @@ class celula(object):
 
 	def get_poza( self ) :
 
-		photo1 = PhotoImage(file='c1.gif')
-		photo0 = PhotoImage(file='c0.gif')
-
 		if self.stare == 1 :
-			return photo1
+			return self.photo1
 		else :
-			return photo0
+			return self.photo0
 
 	def get_stare( self ) :
 		
@@ -61,113 +60,182 @@ class celula(object):
 
 		return self.y
 
-#-----------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
 class tabel(object):
 	
-	def __init__( self , root , nr_x , nr_y ) :
+	def __init__( self , root , nr_randuri , nr_coloane ) :
 
 		self.matrice_celule = []
 
 		self.root = root
 
-		self.nr_x = nr_x
-		self.nr_y = nr_y
+		self.zona_top = Frame( root )
+		self.zona_top.pack( side = TOP )
+
+		self.zona_bot = Frame( root )
+		self.zona_bot.pack( side = BOTTOM )
+
+		self.nr_randuri = nr_randuri # nr randuri
+		self.nr_coloane = nr_coloane # nr coloane
 
 		self.nr_gen = 0
 
-		self.b_next_gen = Button( self.root , text = "next gen" )
-		self.b_next_gen.grid( row = self.nr_x + 1 )
+		self.b_next_gen = Button( self.zona_bot , text = "next gen" )
+		self.b_next_gen.pack()
 
-		#self.b_next_gen["command"] = self.run_next_gen()    <-  se adauga linia dupa tezolvare bug mem mat
+		self.b_next_gen["command"] = self.run_next_gen()  
 
-		for i in range( self.nr_x ) :
+		for i in range( self.nr_randuri ) :
 
-			for j in range( self.nr_y ) :
+			for j in range( self.nr_coloane ) :
 
-				self.matrice_celule.append( celula( randint( 0 , 1 ) , i , j , self.root ) )
+				self.matrice_celule.append( celula( randint( 0 , 1 ) , i , j , self.zona_top ) )
 
-				print( str( i ) + "<- i | j->" + str( j ) + "\n" )
+				#print( str( i ) + "<- i | j->" + str( j ) + "\n" )  <- printeaza i si j
 
 
 
 	def set_celula( self , x , y , stare ) :
 
-		self.matrice_celule[ x * self.nr_y + y ].set_stare = stare
+		self.matrice_celule[ nr_in_lista( x , y ) ].set_stare = stare
 
 	def get_nr_gen( self ) :
 
 		return self.nr_gen
 
-	def next_gen( self , x , y ) :
+	def run_next_gen( self ) :
 
-		vecini = 0
+		self.nr_gen += 1
 
-		if ( x - 1 ) * self.nr_y + y - 1 >= 0 and ( x - 1 ) * self.nr_y + y - 1 < self.nr_x * self.nr_y : # stanga sus
-			if self.matrice_celule[ ( x - 1 ) * self.nr_y + y - 1 ].get_stare == 1 : 
+		for i in range( self.nr_randuri ) :
+
+			for j in range( self.nr_coloane ) :
+
+				self.next_gen( i , j )
+
+
+	'''def next_gen( self , x , y ) :
+
+		vecini = 0             #              x * self.nr_coloane + y
+
+		if ( x - 1 ) * self.nr_coloane + y - 1 >= 0 and ( x - 1 ) * self.nr_coloane + y - 1 < self.nr_randuri * self.nr_coloane : # stanga sus
+			if self.matrice_celule[ ( x - 1 ) * self.nr_coloane + y - 1 ].get_stare == 1 : 
 				vecini += 1
 
-		if ( x - 1 ) * self.nr_y + y >= 0 and ( x - 1 ) * self.nr_y + y < self.nr_x * self.nr_y : # sus
-			if self.matrice_celule[ ( x - 1 ) * self.nr_y + y ].get_stare == 1 :
+		if ( x - 1 ) * self.nr_coloane + y >= 0 and ( x - 1 ) * self.nr_coloane + y < self.nr_randuri * self.nr_coloane : # sus
+			if self.matrice_celule[ ( x - 1 ) * self.nr_coloane + y ].get_stare == 1 :
 				vecini += 1
 
-		if ( x - 1 ) * self.nr_y + y + 1 >= 0 and ( x - 1 ) * self.nr_y + y + 1 < self.nr_x * self.nr_y : # dreapta sus
-			if self.matrice_celule[ ( x - 1 ) * self.nr_y + y + 1 ].get_stare == 1 : 
+		if ( x - 1 ) * self.nr_coloane + y + 1 >= 0 and ( x - 1 ) * self.nr_coloane + y + 1 < self.nr_randuri * self.nr_coloane : # dreapta sus
+			if self.matrice_celule[ ( x - 1 ) * self.nr_coloane + y + 1 ].get_stare == 1 : 
 				vecini += 1
 
-		if x * self.nr_y + y - 1 >= 0 and x * self.nr_y + y - 1 < self.nr_x * self.nr_y : # stanga
-			if self.matrice_celule[ x * self.nr_y + y - 1 ].get_stare == 1 : 
+		if x * self.nr_coloane + y - 1 >= 0 and x * self.nr_coloane + y - 1 < self.nr_randuri * self.nr_coloane : # stanga
+			if self.matrice_celule[ x * self.nr_coloane + y - 1 ].get_stare == 1 : 
 				vecini += 1
 
-		if x * self.nr_y + y + 1 >= 0 and x * self.nr_y + y + 1 < self.nr_x * self.nr_y : # dreapta
-			if self.matrice_celule[ x * self.nr_y + y + 1 ].get_stare == 1 : 
+		if x * self.nr_coloane + y + 1 >= 0 and x * self.nr_coloane + y + 1 < self.nr_randuri * self.nr_coloane : # dreapta
+			if self.matrice_celule[ x * self.nr_coloane + y + 1 ].get_stare == 1 : 
 				vecini += 1
 
-		if ( x + 1 ) * self.nr_y + y - 1 >= 0 and ( x + 1 ) * self.nr_y + y - 1 < self.nr_x * self.nr_y : # stanga jos
-			if self.matrice_celule[ ( x + 1 ) * self.nr_y + y - 1 ].get_stare == 1 : 
+		if ( x + 1 ) * self.nr_coloane + y - 1 >= 0 and ( x + 1 ) * self.nr_coloane + y - 1 < self.nr_randuri * self.nr_coloane : # stanga jos
+			if self.matrice_celule[ ( x + 1 ) * self.nr_coloane + y - 1 ].get_stare == 1 : 
 				vecini += 1
 
-		if ( x + 1 ) * self.nr_y + y >= 0 and ( x + 1 ) * self.nr_y + y < self.nr_x * self.nr_y : # jos
-			if self.matrice_celule[ ( x + 1 ) * self.nr_y + y ].get_stare == 1 :  
+		if ( x + 1 ) * self.nr_coloane + y >= 0 and ( x + 1 ) * self.nr_coloane + y < self.nr_randuri * self.nr_coloane : # jos
+			if self.matrice_celule[ ( x + 1 ) * self.nr_coloane + y ].get_stare == 1 :  
 				vecini += 1
 
-		if ( x + 1 ) * self.nr_y + y + 1 >= 0 and ( x + 1 ) * self.nr_y + y + 1 < self.nr_x * self.nr_y : # deapata jos
-			if self.matrice_celule[ ( x + 1 ) * self.nr_y + y + 1 ].get_stare == 1 : 
+		if ( x + 1 ) * self.nr_coloane + y + 1 >= 0 and ( x + 1 ) * self.nr_coloane + y + 1 < self.nr_randuri * self.nr_coloane : # deapata jos
+			if self.matrice_celule[ ( x + 1 ) * self.nr_coloane + y + 1 ].get_stare == 1 : 
 				vecini += 1
 
-		if self.matrice_celule[ x * self.nr_y + y ].get_stare == 1 : 
+		if self.matrice_celule[ x * self.nr_coloane + y ].get_stare == 1 : 
 
 			if vecini > 3 and vecini < 2 : 
 
-				self.matrice_celule[ x * self.nr_y + y ].set_next_gen( 0 )
+				self.matrice_celule[ x * self.nr_coloane + y ].set_next_gen( 0 )
 
 		else : 
 
 			if vecini == 3 :
 
-				self.matrice_celule[ x * self.nr_y + y ].set_next_gen( 1 )
+				self.matrice_celule[ x * self.nr_coloane + y ].set_next_gen( 1 )'''
+
+	def next_gen( self , x , y ) :
+
+		vecini = self.nr_vecini( x , y )
+
+		if self.matrice_celule[ nr_in_lista( x , y ) ].get_stare == 1 : 
+
+			if vecini > 3 and vecini < 2 : 
+
+				self.matrice_celule[ nr_in_lista( x , y ) ].set_next_gen( 0 )
+
+		else : 
+
+			if vecini == 3 :
+
+				self.matrice_celule[ nr_in_lista( x , y ) ].set_next_gen( 1 )
 
 
-	def run_next_gen( self ) :
+	def nr_in_lista( self , x , y ) : 
 
-		self.nr_gen += 1
+	 	return	x * self.nr_coloane + y
 
-		for i in range( self.nr_x ) :
+	def nr_vecini( self , x , y ) :
 
-			for j in range( self.nr_y ) :
+		vecini = 0
 
-				self.next_gen( i , j )
+		if x - 1 >= 0 and y - 1 >= 0 : # stanga sus
+			if self.matrice_celule[ self.nr_in_lista( x - 1 , y - 1 ) ].get_stare == 1 :
+				vecini += 1
+
+		if x - 1 >= 0 : # sus
+			if self.matrice_celule[ self.nr_in_lista( x - 1 , y ) ].get_stare == 1 :
+				vecini += 1
+
+		if x - 1 >= 0 and y + 1 < self.nr_coloane  : # dreapta sus
+			if self.matrice_celule[ self.nr_in_lista( x - 1 , y + 1 ) ].get_stare == 1 :
+				vecini += 1
+
+		if y - 1 >= 0 : # stanga
+			if self.matrice_celule[ self.nr_in_lista( x , y - 1 ) ].get_stare == 1 :
+				vecini += 1
+
+		if y + 1 < self.nr_coloane : # dreapta
+			if self.matrice_celule[ self.nr_in_lista( x , y + 1 ) ].get_stare == 1 :
+				vecini += 1
+
+		if x + 1 < nr_randuri and y - 1 >= 0 : # stanga jos
+			if self.matrice_celule[ self.nr_in_lista( x + 1 , y - 1 ) ].get_stare == 1 :
+				vecini += 1
+
+		if x + 1 < nr_randuri : # jos
+			if self.matrice_celule[ self.nr_in_lista( x + 1 , y ) ].get_stare == 1 :
+				vecini += 1
+
+		if x + 1 < nr_randuri and y + 1 < self.nr_coloane : # stanga jos
+			if self.matrice_celule[ self.nr_in_lista( x + 1 , y + 1 ) ].get_stare == 1 :
+				vecini += 1
+
+		return vecini
+
+
+
+
 
 
 
 		
 # start ------------------------------------------------------------------------------- start ----------------
 
-a = 2 # randuri
+a = 10 # randuri
 
-b = 3 # coloane
+b = 15 # coloane
 
-dim = str( b * 50 + 25 ) + "x" + str( a * 60 + 30 )
+dim = str( b * 55 + 25 ) + "x" + str( a * 55 + 30 )
 
 root = Tk()
 
